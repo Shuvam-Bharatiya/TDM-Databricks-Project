492c531b339ba8825ed5de93666d0191e1d0cae0
Shuvam-Bharatiya <shuvambharatiya_official@outlook.com>
2026-06-11 13:25:57 +0530

    Update 02_Data_Profilling.sql

--- SQL_Scripts/02_Data_Profilling.sql
+++ SQL_Scripts/02_Data_Profilling.sql
@@ -1,1 +1,13 @@
-
+-- Total records
+SELECT COUNT(*) AS total_records
+FROM customer;
+-- Null values
+SELECT COUNT(*) AS null_names
+FROM customer
+WHERE customer_name IS NULL;
+-- Duplicate customer IDs
+SELECT customer_id,
+       COUNT(*)
+FROM customer
+GROUP BY customer_id
+HAVING COUNT(*) > 1;
