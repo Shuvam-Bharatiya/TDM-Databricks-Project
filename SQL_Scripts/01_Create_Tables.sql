05ae25048eef7297317186ce7a457ffbb5ab6e4d
Shuvam-Bharatiya <shuvambharatiya_official@outlook.com>
2026-06-11 13:24:05 +0530

    Update 01_Create_Tables.sql

--- SQL_Scripts/01_Create_Tables.sql
+++ SQL_Scripts/01_Create_Tables.sql
@@ -1,1 +1,13 @@
-
+CREATE TABLE customer (
+  customer_id INT,
+  customer_name STRING,
+  email STRING,
+  phone STRING,
+  city STRING
+  );
+CREATE TABLE subscription (
+  subscription_id INT,
+  customer_id INT,
+  plan_name STRING,
+  status STRING
+  );
