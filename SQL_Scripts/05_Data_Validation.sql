1683414284cadb80d8f2fb0dd295f01ba991fef2
Shuvam-Bharatiya <shuvambharatiya_official@outlook.com>
2026-06-11 13:29:17 +0530

    Update 05_Data_Validation.sql

--- SQL_Scripts/05_Data_Validation.sql
+++ SQL_Scripts/05_Data_Validation.sql
@@ -1,1 +1,12 @@
-
+-- Duplicate validation
+SELECT customer_id,
+COUNT(*)
+FROM customer
+GROUP BY customer_id
+HAVING COUNT(*) > 1;
+-- Referential Integrity
+SELECT *
+FROM subscription s
+LEFT JOIN customer c
+ON s.customer_id = c.customer_id
+WHERE c.customer_id IS NULL;
