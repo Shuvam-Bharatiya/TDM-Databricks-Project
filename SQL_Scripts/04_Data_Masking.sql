6050e8a05b7f429a72f560442db2c5fb9304f387
Shuvam-Bharatiya <shuvambharatiya_official@outlook.com>
2026-06-11 13:28:06 +0530

    Update 04_Data_Masking.sql

--- SQL_Scripts/04_Data_Masking.sql
+++ SQL_Scripts/04_Data_Masking.sql
@@ -1,1 +1,12 @@
-
+SELECT
+customer_id,
+CONCAT(
+SUBSTRING(customer_name,1,1),
+'***'
+) AS masked_name,
+CONCAT(
+SUBSTRING(phone,1,2),
+'XXXXXX',
+SUBSTRING(phone,9,2)
+) AS masked_phone
+FROM customer;
